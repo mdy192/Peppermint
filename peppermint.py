bb9604488ca964a09238da75e9c25ef595126863
mdy192 <mrdy2004@gmail.com>
2024-11-06 14:52:57 -0600

    Comments Update to peppermint.py
    Added some comments to each process + Thank you note :)

--- peppermint.py
+++ peppermint.py
@@ -3,13 +3,15 @@
 import os
 import time
 
-def text2dict(text):
-    text_dict = []
+def text2list(text):
+    #Turns the user's text and puts it into a list, seperated by each character
+    text_list = []
     for char in text:
-        text_dict.append(ord(char))
-    return text_dict
+        text_list.append(ord(char))
+    return text_list
 
 def seperate_under_127(x):
+    #Seperates a long character string into pieces that are ord() 127 or below
     result = []
     i = 0
 
@@ -28,9 +30,12 @@ def seperate_under_127(x):
     return result
     
 def peppermintEncrypt():
+    # Prepares the terminal for Encrypting!
     os.system('cls')
     print("PeppermintEncrypt Chosen")
     
+    # Obtains cleartext from user & converting the time into a 6-digit code
+
     user_cleartext = input("Enter Cleartext: ")
     timenow = int(time.time())
     time_hour = timenow // 3600
@@ -38,12 +43,14 @@ def peppermintEncrypt():
     for num in str(time_hour):
         time_hour_split.append(num)
 
+    # Creates a new list where the two lists (cleartext & 6-digit time code) will be combined, 
+    # via alternating and the shorter list being repeated when there are insufficient characters
     combined_list = []
 
-    TextInDict = text2dict(user_cleartext)
+    TextInList = text2list(user_cleartext)
 
-    longer_list = TextInDict if len(TextInDict) >= len(time_hour_split) else time_hour_split
-    shorter_list = time_hour_split if len(TextInDict) >= len(time_hour_split) else TextInDict
+    longer_list = TextInList if len(TextInList) >= len(time_hour_split) else time_hour_split
+    shorter_list = time_hour_split if len(TextInList) >= len(time_hour_split) else TextInList
 
     for i in range(len(longer_list)):
             combined_list.append(longer_list[i])
@@ -58,15 +65,19 @@ def peppermintEncrypt():
 
     peppermint_encoded_list = seperate_under_127(single_element)
 
+    # Turns the new string values into ASCII text
+
     ciphertext_str = [chr(num) for num in peppermint_encoded_list]
 
     ciphertext = ''.join(ciphertext_str)
 
     print(f"Save the code: {time_hour}")
     print(f"Your Ciphertext: \n {ciphertext}")
+    print("""Thank you for using Peppermint
+          🦌⋆꙳•❅*• •*❆ ₊⋆✮⋆˙""")
 
 def peppermintDecrypt():
-    
+    # Prepares the terminal for Peppermint Decryption!
     os.system('cls')
     print("PeppermintDecrypt Chosen")
 
@@ -75,6 +86,8 @@ def peppermintDecrypt():
     user_ciphertext = (input("Please enter the ciphertext: \n"))
 
     user_code = (input("Please enter code: \n"))
+
+    # Seperates the characters in the encoded text
 
     for char in user_ciphertext:
         user_ciphertext_list_ord.append(ord(char))
